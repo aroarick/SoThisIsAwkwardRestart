24771f57d7e84979e040077fd6a121bbaec1e8f4
joey kessler <joeykessler@gmail.com>
2019-12-02 21:27:09 -0600

    Cleaned up home page positioning

--- stia/src/components/Home/home.js
+++ stia/src/components/Home/home.js
@@ -11,84 +11,27 @@ class Home extends React.Component {
   }
   render() {
     return (
-      <Row style={{ height: "100%" }}>
-        {/* <Col md={5} className="column-1"> */}
-        {/* <Row className="row-1">
-            <Col id="title" md={12}> */}
-        {
-          <Col sm={6} style={{ height: "100%" }}>
-            <Image id="logo-image" src="images/logo-dark-cropped.PNG" />
-          </Col>
-        }
-        {
-          <Col sm={6}>
-            <Row>
-              <Button className="btn btn-home-custom help" href="/help">
-                Help Me Help You
-              </Button>
-            </Row>
-            <Row>
-              <Button className="btn btn-home-custom login" href="/login">
-                Login Or Sign Up
-              </Button>
-            </Row>
-            <Row>
-              <Button
-                className="btn btn-home-custom testimonials"
-                href="/testimonials"
-              >
-                Testimonials
-              </Button>
-            </Row>
-            <Row>
-              <Button className="btn btn-home-custom about" href="/about">
-                FAQ
-              </Button>
-            </Row>
-          </Col>
-        }
-
-        {/* </Col>
-          </Row> */}
-        {/* <Row className="row-2"> */}
-        {/* <Col md={12} className="color-block-1"></Col> */}
-        {/* </Row>
-        </Col> */}
-        {/* <Col md={4} className="column-2">
-          <Row className="row-3"> */}
-        {/* <Col md={12} className="color-block-2"></Col> */}
-        {/* </Row>
-          <Row className="row-4">
-            <Col md={12}> */}
-        {/*  */}
-        {/* </Col>
-          </Row>
-        </Col> */}
-        {/* <Col md={3} className="column-3">
-          <Row className="row-5">
-            <Col md={12}> */}
-        {/*  */}
-        {/* </Col>
-          </Row>
-          <Row className="row-6">
-            <Col md={12} className="color-block-3"></Col>
-          </Row>
-          <Row className="row-7">
-            <Col md={12}> */}
-        {/*  */}
-        {/* </Col>
-          </Row>
-          <Row className="row-8">
-            <Col md={12} className="color-block-4"></Col>
-          </Row>
-          <Row className="row-9">
-            <Col md={12}> */}
-        {/* <Button className="btn btn-home-custom about" href="/about">
-                FAQ
-              </Button> */}
-        {/* </Col>
-          </Row>
-        </Col> */}
+      <Row>
+        <Col md={6}>
+          <Image id="logo-image" src="images/logo-dark-cropped.PNG" fluid />
+        </Col>
+        <Col md={6}>
+          <Button className="btn btn-home-custom help" href="/help">
+            Help Me Help You
+          </Button>
+          <Button className="btn btn-home-custom login" href="/login">
+            Login Or Sign Up
+          </Button>
+          <Button
+            className="btn btn-home-custom testimonials"
+            href="/testimonials"
+          >
+            Testimonials
+          </Button>
+          <Button className="btn btn-home-custom about" href="/about">
+            FAQ
+          </Button>
+        </Col>
       </Row>
     );
   }
