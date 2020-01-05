27ffb04b266cca486770c6ec9f93df40e9929bb1
Ashley Roarick <ahorseroar@gmail.com>
2020-01-05 16:34:22 -0600

    finished optout backend

--- serverside/message.js
+++ serverside/message.js
@@ -1,6 +1,9 @@
 module.exports = {
   postMessage: function(request, response) {
     const sgMail = require("@sendgrid/mail");
+    var config = require("./db");
+    var mysql = require("mysql");
+
     const key = process.env.SENDGRID_API_KEY;
 
     sgMail.setApiKey(key);
@@ -9,19 +12,46 @@ module.exports = {
       to: request.body.emailAddress,
       from: "noreply@sothisisawkward.com",
       subject: request.body.subject,
-      // firstThing: request.body.firstThing,
       text: request.body.textMessage,
       html: request.body.htmlMessage
     };
 
-    sgMail
-      .send(msg)
-      .then(function(value) {
-        response.json({ response: "good" });
-      })
-      .catch(function(error) {
-        response.json({ response: "bad", error: error });
-        console.log(key);
-      });
+    let allGood = false;
+
+    var con = mysql.createConnection(config.dbConfig);
+    con.connect(function(err) {
+      if (err) {
+        response.json({ response: "bad", error: err });
+      } else {
+        var sql =
+          "SELECT COUNT(*) AS count FROM `optout` WHERE email='" +
+          request.body.emailAddress +
+          "'";
+        con.query(sql, function(err, result, fields) {
+          if (err) {
+            response.json({ response: "bad", error: err });
+          } else {
+            console.log(result);
+            if (result[0].count == 0) {
+              allGood = true;
+            }
+
+            if (allGood) {
+              sgMail
+                .send(msg)
+                .then(function(value) {
+                  response.json({ response: "good" });
+                })
+                .catch(function(error) {
+                  response.json({ response: "bad", error: error });
+                  console.log(key);
+                });
+            } else {
+              response.json({ response: "bad", error: "This user opted out" });
+            }
+          }
+        });
+      }
+    });
   }
 };
