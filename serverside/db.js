c93991614f6b8ec408a2c2d275fffe51449da401
Ashley Roarick <ahorseroar@gmail.com>
2020-01-05 16:07:05 -0600

    optout

--- serverside/db.js
+++ serverside/db.js
@@ -1,14 +1,8 @@
-module.exports = function() {
-  this.connection = function() {
-    var mysql = require("mysql");
-
-    var con = mysql.createConnection({
-      host: "db.sothisisawkward.com",
-      user: "stiadbuser",
-      password: "s00thinG",
-      database: "sothisisawkward"
-    });
-
-    return con;
-  };
+module.exports = {
+  dbConfig: {
+    host: "db.sothisisawkward.com",
+    user: "stiadbuser",
+    password: "s00thinG",
+    database: "sothisisawkward"
+  }
 };
--- serverside/optout.js
+++ serverside/optout.js
@@ -3,26 +3,18 @@ module.exports = {
     let email = request.query.email;
 
     var mysql = require("mysql");
+    var config = require("./db");
 
-    console.log("Howdy");
     if (email != undefined || email.length > 0) {
-      var con = mysql.createConnection({
-        host: "db.sothisisawkward.com",
-        user: "stiadbuser",
-        password: "s00thinG",
-        database: "sothisisawkward",
-        debug: true
-      });
+      var con = mysql.createConnection(config.dbConfig);
 
       con.connect(function(err) {
         if (err) {
-          //   throw err;
           response.json({ response: "bad", error: err });
         } else {
           var sql = "INSERT INTO optout (email) VALUES ('" + email + "')";
           con.query(sql, function(err, result, fields) {
             if (err) {
-              //   throw err;
               response.json({ response: "bad", error: err });
             } else {
               response.json({ response: "good" });
@@ -33,7 +25,5 @@ module.exports = {
     } else {
       response.json({ response: "bad", error: "no email received" });
     }
-
-    console.log(email);
   }
 };
